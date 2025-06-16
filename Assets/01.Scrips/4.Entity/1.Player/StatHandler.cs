b02cb776928d4d57fe5ea4e8dadb8b73cd4a4abf
garamzui <forsave121@gmail.com>
2025-06-16 17:34:22 +0900

    typo Refactor: 스탯 데이터 스트링변환 함수 수정

--- Assets/01.Scrips/4.Entity/1.Player/StatHandler.cs
+++ Assets/01.Scrips/4.Entity/1.Player/StatHandler.cs
@@ -9,13 +9,13 @@ public class StatHandler : MonoBehaviour,IBattleEntity
 {
     public StatData statData;
     private Dictionary<StatType, int> currentStats = new Dictionary<StatType, int>();
-    public Dictionary<StatType, int>  serializeStats = new Dictionary<StatType, int>();// 세이브용 스탯 저장할 딕셔너리
+    public Dictionary<string, string>  serializeStats = new Dictionary<string, string>();// 세이브용 스탯 저장할 딕셔너리
     
     public PlayerInfo playerInfo;
     private void Awake()
     {
         InitializeStats();
-        
+       
     }
 
     private void InitializeStats()
@@ -57,15 +57,21 @@ public class StatHandler : MonoBehaviour,IBattleEntity
         currentStats[statType] = setvalue;
     }
     
-    public Dictionary<string, float> ToStatDict()
+    //죄송해요 애초에 다 스트링으로 해야되는데 잘못만들었습니다. 
+    //요기에 만들어둔 스트링 딕셔너리는 사용하셔도되고 안하셔도됩니다. 
+    //키값 = 타입 밸류값 = 스탯값 요렇게 들어가게 해두었습니다 .
+    // serializeStats(또는 세이브쪽에서 사용하실 딕셔너리 만드셔서) =ToStarDict();
+    //스탯 핸들러자체는 GameManager -> player -> statHandler 요렇게 캐싱해 두었습니다.
+    public Dictionary<string, string> ToStatDict()
     {
-        var dict = new Dictionary<string, float>();
+        var dict = new Dictionary<string, string>();
         foreach (StatType stat in Enum.GetValues(typeof(StatType)))
         {
-            dict[stat.ToString()] = GetStat(stat);
+            dict[stat.ToString()] = GetStat(stat).ToString();
         }
         return dict;
     }
+    
 /*============================================================================================*/
 
     public IEnumerator ActionOnTurn(BattlePhase phase)
