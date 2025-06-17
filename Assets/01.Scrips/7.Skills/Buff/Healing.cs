3707cf319e43afb4a8d8ccc969b716fa0bcdd9e7
Yusang-1 <206505633+Yusang-1@users.noreply.github.com>
2025-06-17 19:34:53 +0900

    feat : 힐 버프, 스킬

---
+++ Assets/01.Scrips/7.Skills/Buff/Healing.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class Healing : IBuff
+{
+    private int buffCount = 1;
+
+    public bool Execute(IBattleEntity entity) //주사위 눈금 합의 2배만큼의 비율 회복
+    {
+        if (buffCount == 0) return false;
+        buffCount--;
+
+        EntityInfo info = entity.GetEntityInfo();
+
+        int[] diceNum = SkillManager.instance.RollDice();
+        int healRatio = diceNum.Sum() * 2;
+        
+        info.currentHp = Mathf.Clamp(info.currentHp + (info.currentHp * healRatio), 0, info.maxHp);
+
+        return true;
+    }
+
+    public IBuff Clone()
+    {
+        return new Healing();
+    }
+}
