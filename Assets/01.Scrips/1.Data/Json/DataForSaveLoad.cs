5d6ca6332499902a869a60adc774e512170f7e6e
Yusang-1 <206505633+Yusang-1@users.noreply.github.com>
2025-06-17 12:36:16 +0900

    feat : 인벤토리, 퀘스트 저장 기능 추가

--- Assets/01.Scrips/1.Data/Json/DataForSaveLoad.cs
+++ Assets/01.Scrips/1.Data/Json/DataForSaveLoad.cs
@@ -1,39 +1,58 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class DataForSaveLoad : MonoBehaviour
+public class DataForSaveLoad
 {
     [SerializeField] Dictionary<string, string> playerStats;
-
+    [SerializeField] List<IItem> InventoryData;
+    [SerializeField] List<QuestData>[] questData = new List<QuestData>[2];
+    
     public DataForSaveLoad GetSaveData()
     {
         playerStats = GameManager.Instance.player.statHandler.ToStatDict();
+        InventoryData = UIManager.Instance.inventory.items.ToList();
+        questData[0] = QuestManager.Instance.PlayerQuests.ToList();
+        questData[1] = QuestManager.Instance.CompletedQuests.ToList();       
+
         return this;
     }
 
     public void GetLoadData(string jsonString)
     {
-        //GameManager.Instance.player.statHandler.serializeStats = JsonConvert.DeserializeObject<Dictionary<StatType,int>>(jsonString); ////방법 2
-
         JObject root = JObject.Parse(jsonString); //방법 1
 
         JToken player = root["playerStats"];
         LoadPlayer(player);
 
-        JToken inven = root["inventory"]; //아마 이런식?
+        JToken inven = root["InventoryData"];
+        LoadInventory(inven);
+
+        JToken quest = root["questData"];
+        LoadQuest(quest);
     }
 
-    public void LoadPlayer(JToken Data)
+    private void LoadPlayer(JToken Data)
     {
-        int i = 0;
+        //GameManager.Instance.player.statHandler.serializeStats = JsonConvert.DeserializeObject<Dictionary<string,string>>(Data.ToString()); ////방법 2
         foreach(JProperty jj in Data)
         {
             GameManager.Instance.player.statHandler.serializeStats[jj.Name] = jj.Value.ToString();
-            i++;
         }
 
         GameManager.Instance.player.statHandler.LoadStatsToCurrent();
+    }
+
+    private void LoadInventory(JToken Data)
+    {
+        UIManager.Instance.inventory.items = JsonConvert.DeserializeObject<List<IItem>>(Data.ToString()).ToList();        
+    }
+
+    private void LoadQuest(JToken Data)
+    {
+        QuestManager.Instance.PlayerQuests = JsonConvert.DeserializeObject<List<QuestData>>(Data[0].ToString()).ToList();
+        QuestManager.Instance.CompletedQuests = JsonConvert.DeserializeObject<List<QuestData>>(Data[1].ToString()).ToList();
     }
 }
