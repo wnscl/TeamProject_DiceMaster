9c45bdccff5bc7c7cdd498e6b7ea84d45d1411c3
garamzui <forsave121@gmail.com>
2025-06-18 12:21:52 +0900

    Feat : npc와 상호작용 구현

--- Assets/01.Scrips/4.Entity/1.Player/Player.cs
+++ Assets/01.Scrips/4.Entity/1.Player/Player.cs
@@ -3,24 +3,25 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 
-
-public class Player : MonoBehaviour,IBattleEntity
+public class Player : MonoBehaviour, IBattleEntity, IInteractable
 {
     public StatHandler statHandler;
     public PlayerInfo playerInfo;
+    public GameObject interactableSign;
 
     private void Awake()
     {
         statHandler = GetComponent<StatHandler>();
     }
+
     public IEnumerator ActionOnTurn(BattlePhase phase)
     {
         Debug.Log("Player's turn action executed.");
         return null;
     }
-
 
 
     public void GetDamage(int dmg)
@@ -46,16 +47,70 @@ public class Player : MonoBehaviour,IBattleEntity
         {
             isDead = true;
         }
-        ;
 
+        ;
     }
 
     public EntityInfo GetEntityInfo()
     {
         return playerInfo;
     }
-    
-    
 
 
+    private IEnumerator interCoroutine;
+
+    private IEnumerator interact(IInteractable inpc)
+    {
+        yield return new WaitUntil(() => interTrigger);
+        inpc.OnInteract();
+    }
+
+    private bool isInteractable = false;
+    private bool interTrigger = false;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other is IInteractable inpc)
+        {
+            interactableSign.SetActive(true);
+            isInteractable = true;
+            interCoroutine = interact(inpc);
+            if (interCoroutine == null)
+            {
+                interCoroutine = interact(inpc);
+                StartCoroutine(interCoroutine);
+            }
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other is IInteractable)
+        {
+            interactableSign.SetActive(false);
+            isInteractable = false;
+            interTrigger = false;
+            if (interCoroutine != null)
+            {
+                StopCoroutine(interCoroutine);
+                interCoroutine = null;
+            }
+        }
+    }
+
+    public void OnInterTry(InputAction.CallbackContext context)
+    {
+        if (!isInteractable) return;
+
+        if (context.phase == InputActionPhase.Performed)
+        {
+           interTrigger = true;
+        }
+    }
+
+    
+
+    public void OnInteract()
+    {
+    }
 }
